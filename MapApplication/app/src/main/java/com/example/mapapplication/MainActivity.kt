9b1535c9844f5b644f456cbe25b6d292234e8db5
seldon1128 <seldon1128@gmail.com>
2024-09-19 11:59:56 -0600

    Reemplazando todo el contenido del repositorio

---
+++ MapApplication/app/src/main/java/com/example/mapapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.mapapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.inicio)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainScreen)) { v, insets -> //main en mainScreen para la pantalla de LogIn
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ dibujo/app/src/main/java/com/example/dibujo/dibujo.java
@@ -0,0 +1,11 @@
+package com.example.dibujo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class dibujo extends Activity{
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
